9a06f2c81c79983731aa1e4d8b38ae83226f0ad4
Himanshu Gahlot <himashu@listenfirstmedia.com>
2026-03-18 12:01:44 +0530

    chore: remove ignored files from repo

--- dist/prisma.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-const client_1 = require("@prisma/client");
-let prisma;
-if (!global.__prisma) {
-    global.__prisma = new client_1.PrismaClient();
-}
-exports.prisma = prisma = global.__prisma;
